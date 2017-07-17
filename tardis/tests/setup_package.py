65437e06faf78134793919338020fc4524330de0
Vaibhav Gupta <vg3095@gmail.com>
2017-07-17 10:27:30 +0530

    Include csv files for tests

--- tardis/tests/setup_package.py
+++ tardis/tests/setup_package.py
@@ -2,5 +2,6 @@ def get_package_data():
     return {
         _ASTROPY_PACKAGE_NAME_ + '.tests': ['coveragerc', 'data/*.h5',
                                             'data/*.dat', 'data/*.npy',
+                                            'data/*csv',
                                             'integration_tests/*/*.yml',
                                             'integration_tests/*/*.dat']}
